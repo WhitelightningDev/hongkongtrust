75c90ab9021d28c64650b6b589914704528d3101
Daniel Mommsen <danielmommsen@hotmail.com>
2025-08-28 13:30:53 +0200

    `Added AuthService and initialized authentication in App component`

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./components/footer/footer";
+import { AuthService } from './interceptors/auth.service';
 
 
 @Component({
@@ -9,6 +10,17 @@ import { Footer } from "./components/footer/footer";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'hongkongtrust';
+
+  constructor(private authService: AuthService) {}
+
+  async ngOnInit() {
+    try {
+      await this.authService.initAuth();
+      console.log('Authentication initialized successfully.');
+    } catch (error) {
+      console.error('Failed to initialize authentication:', error);
+    }
+  }
 }
